b09db8a4d0373ef8c7b34e3abb7e6dac8a31b047
Nadezhda Kireyenka <70837252+NadieKir@users.noreply.github.com>
2022-12-08 13:57:25 +0300

    Complete call, apply, bind

--- this.test.js
+++ this.test.js
@@ -1,39 +1,33 @@
 describe('THIS', () => {
   describe('call, apply, bind', () => {
-    it('Invoke function with specific this', () => {
-      // DON'T CHANGE
+    it('Invokes function with specific this', () => {
       const a = { msg: 'aaa' };
       const b = { msg: 'bbb' };
 
-      // DON'T CHANGE
       function example() {
         return this.msg + ' ' + this.msg;
       }
 
-      // TODO: write correct expression inside expect
-      expect(/* ??? */).toBe('aaa aaa');
-      expect(/* ??? */).toBe('bbb bbb');
+      expect(example.call(a)).toBe('aaa aaa');
+      expect(example.bind(b)()).toBe('bbb bbb');
     });
 
-    it('Creates function  connected with specific this', () => {
-      // DON'T CHANGE
+    it('Creates function connected with specific this', () => {
       function fn() {
         return this.name;
       }
-      // DON'T CHANGE
+
       const tom = { name: 'Tom', age: 22 };
       const bob = { name: 'Bob', get: 50 };
 
-      // TODO: fix
-      const getTomName = fn;
-      const getBobName = fn;
+      const getTomName = fn.bind(tom);
+      const getBobName = fn.bind(bob);
 
       expect(getTomName()).toBe('Tom');
       expect(getBobName()).toBe('Bob');
     });
 
-    test('Function from object method. Fix me', () => {
-      // DON'T CHANGE
+    it('Invokes function from object method', () => {
       const person = {
         firstName: 'Ivan',
         secondName: 'Ivanov',
@@ -44,68 +38,66 @@ describe('THIS', () => {
         },
       };
 
-      // TODO: FIX ME
-      const sayHello = person.sayHello;
-
-      expect(sayHello()).toBe(person.sayHello());
+      const sayHelloToPerson = person.sayHello.bind(person);
+      expect(sayHelloToPerson()).toBe(person.sayHello());
     });
 
-    describe('Math.max for array ', () => {
-      describe('using call', () => {
-        const findMax = (arr) => {
-          // TODO: fix me
-          return Math.max(arr);
-        };
+    // describe('Math.max for array ', () => {
+    //   describe('using call', () => {
+    //     const findMax = (arr) => {
+    //       // TODO: fix me
+    //       return Math.max(arr);
+    //     };
 
-        it('Finds max number in array', () => {
-          expect(findMax([1, 3, 5, 2])).toBe(5);
-        });
-      });
+    //     it('Finds max number in array', () => {
+    //       expect(findMax([1, 3, 5, 2])).toBe(5);
+    //     });
+    //   });
 
-      describe('using apply', () => {
-        const findMax = (arr) => {
-          // TODO: fix me
-          return Math.max(arr);
-        };
+    //   describe('using apply', () => {
+    //     const findMax = (arr) => {
+    //       // TODO: fix me
+    //       return Math.max(arr);
+    //     };
 
-        it('Finds max number in array', () => {
-          expect(findMax([1, 3, 5, 2])).toBe(5);
-        });
-      });
+    //     it('Finds max number in array', () => {
+    //       expect(findMax([1, 3, 5, 2])).toBe(5);
+    //     });
+    //   });
 
-      describe('using spread ...', () => {
-        const findMax = (arr) => {
-          // TODO: fix me
-          return Math.max(arr);
-        };
+    //   describe('using spread ...', () => {
+    //     const findMax = (arr) => {
+    //       // TODO: fix me
+    //       return Math.max(arr);
+    //     };
 
-        it('Finds max number in array', () => {
-          expect(findMax([1, 3, 5, 2])).toBe(5);
-        });
-      });
-    });
+    //     it('Finds max number in array', () => {
+    //       expect(findMax([1, 3, 5, 2])).toBe(5);
+    //     });
+    //   });
+    // });
   });
 
-  describe('arrow functions', () => {
-    test('Rewrite test to find mistake and fix implementation', () => {
-      const person = {
-        firstName: 'Ivan',
-        secondName: 'Ivanov',
-        age: 20,
+  // describe('arrow functions', () => {
+  //   test('Rewrite test to find mistake and fix implementation', () => {
+  //     const person = {
+  //       firstName: 'Ivan',
+  //       secondName: 'Ivanov',
+  //       age: 20,
 
-        sayHello: () => {
-          return `Hi, ${this.firstName}!`;
-        },
-      };
+  //       sayHello: () => {
+  //         return `Hi, ${this.firstName}!`;
+  //       },
+  //     };
 
-      const sayHello = person.sayHello.bind(person);
+  //     const sayHello = person.sayHello.bind(person);
 
-      // In this example assertion is true, but our code has a bug. Change assertion (toBe part) to find bug (test should fail)
-      expect(sayHello()).toBe(person.sayHello());
-    });
+  //     // In this example assertion is true, but our code has a bug. Change assertion (toBe part) to find bug (test should fail)
+  //     expect(sayHello()).toBe(person.sayHello());
+  //   });
 
-    test.todo(
-      'Rewrite previous exercise by creating the Person class with sayHello method using arrow function, write needed tests.'
-    );
-  });
+  //   test.todo(
+  //     'Rewrite previous exercise by creating the Person class with sayHello method using arrow function, write needed tests.'
+  //   );
+  // });
 });
